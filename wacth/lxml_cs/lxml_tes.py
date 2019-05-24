990261a3331d2c0d95c2cf6619f995127aaa4d63
kidword <1564636097@qq.com>
2019-05-24 18:33:37 +0800

    Default

--- wacth/lxml_cs/lxml_tes.py
+++ wacth/lxml_cs/lxml_tes.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 
-from Watch_ali.lxml_cs import lxml_xpath
+from wacth.lxml_cs import lxml_xpath
 from lxml import etree
 
 html = etree.HTML(lxml_xpath.strs)
 img_info = html.xpath("//div[@class='item']/div[@class='has-sku-image']/a/text()")  # 可选颜色  4 Colors Available
 img_url = html.xpath("//div[@class='pic']/a/@href")
 image_url = [i.replace("//", "") for i in img_url]    # 得到图片链接
-content = html.xpath("//div[@class='info']/span[@class='separator']")   # 介绍内容
-print(len(content))
+content = html.xpath("//*[@id='hs-below-list-items']/li[1]/div/div[3]/h3/a/text()[1]/text()")   # 介绍内容
+print(content)
+
+
