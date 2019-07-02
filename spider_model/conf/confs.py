a3dc81f51c41974296dfd9a86c74616dbe2de9b5
kidword <1564636097@qq.com>
2019-07-02 18:32:01 +0800

    增加redis、增加http请求类

--- spider_model/conf/confs.py
+++ spider_model/conf/confs.py
@@ -6,3 +6,14 @@ MYSQL_PORT = 3306
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'hh226752'
 MYSQL_DATABASE = 'flightradar24'
+
+# redis
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
+REDIS_KEY = 'home'
+PROXY_POOL_URL = 'http://127.0.0.1:5555/random'
+
+# 其他
+TIMEOUT = 10
+MAX_FAILED_TIME = 20
